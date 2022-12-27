ebb47a3c2fe3f9329f196ee036551e78d0668d66
Poonji <poonjij21@gmail.com>
2022-12-28 00:13:21 +0530

    27 DEC

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Welcome to Spotify")
+console.log("Welcome to Spotify");
+let songIndex=0;
+let audioElement=new Audio('song.mp3');
+let masterPlay = document.getElementById('masterplay');
+let myProgressBar=document.getElementById('myProgressBar');
+let songs=[
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+]
+//audioElement.play(); 
+//Handle play/pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play()
+    }
+})
+//Listen to events
+document.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+})
