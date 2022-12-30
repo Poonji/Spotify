4442acc3b42069e5cccb2a52592eb5baf6f50a8a
Poonji <poonjij21@gmail.com>
2022-12-31 00:36:35 +0530

    same

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ let audioElement=new Audio('song.mp3');
 let masterPlay = document.getElementById('masterplay');
 let myProgressBar=document.getElementById('myProgressBar');
 let songs=[
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
-    {songName:"Tum HO", filepath:"song/song.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
+    {songName:"Tum HO", filepath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\song.mp3",coverPath:"C:\Users\hp\OneDrive\Documents\mini project\spotify clone\cover.jpg"},
 ]
 //audioElement.play(); 
 //Handle play/pause click
 masterPlay.addEventListener('click',()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play()
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
     }
 })
 //Listen to events
